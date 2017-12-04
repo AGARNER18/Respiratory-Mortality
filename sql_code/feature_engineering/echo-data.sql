376fe76ee63bb35c4153b972b755fcf1010125e6
Amber Garner <ambermgarner85stat@gmail.com>
2017-12-04 02:31:15 -0600

    Update echo-data.sql

--- sql_code/feature_engineering/echo-data.sql
+++ sql_code/feature_engineering/echo-data.sql
@@ -1,7 +1,3 @@
--- This code extracts structured data from echocardiographies
--- You can join it to the text notes using ROW_ID
--- Just note that ROW_ID will differ across versions of MIMIC-III.
-
 DROP MATERIALIZED VIEW IF EXISTS ECHODATA CASCADE;
 CREATE MATERIALIZED VIEW ECHODATA AS
 select ROW_ID
